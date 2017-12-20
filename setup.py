f66d0112efd6fb29197be7de94c72d4cc773273b
Necdet Can Ateşman <can.atesman@strg.at>
2017-12-20 16:43:14 +0100

    Release: 0.0.2

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ with open(os.path.join(here, 'README.rst')) as f:
 
 setup(
     name='score.sa.orm',
-    version='0.0.1',
+    version='0.0.2',
     description='ORM layer for The SCORE Framework',
     long_description=README,
     author='strg.at',
