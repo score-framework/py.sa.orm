35fc61a6b39e515b589099ea778698643356bd98
Necdet Can Ateşman <can@atesman.at>
2018-02-22 16:16:55 +0100

    Release: 0.0.3

--- score/sa/orm/__init__.py
+++ score/sa/orm/__init__.py
@@ -30,7 +30,7 @@ from ._session import QueryIdsMixin
 from .dataloader import load_data
 from .helpers import create_collection_class, create_relationship_class
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 __all__ = ('create_base', 'init', 'ConfiguredSaOrmModule', 'cls2tbl', 'tbl2cls',
            'IdType', 'QueryIdsMixin', 'load_data', 'create_collection_class',
