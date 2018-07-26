75bd35ecdf349ddd49330f79acfbb1e6b87cdc75
Necdet Can Ateşman <can@atesman.at>
2018-07-26 17:07:52 +0200

    Release: 0.0.7

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright © 2015-2017 STRG.AT GmbH, Vienna, Austria
+# Copyright © 2015-2018 STRG.AT GmbH, Vienna, Austria
 #
 # This file is part of the The SCORE Framework.
 #
@@ -34,7 +34,7 @@ with open(os.path.join(here, 'README.rst')) as f:
 
 setup(
     name='score.sa.orm',
-    version='0.0.6',
+    version='0.0.7',
     description='ORM layer for The SCORE Framework',
     long_description=README,
     author='strg.at',
